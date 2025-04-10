a2ccb70e72a5b4f6bac24254163ef0a60fa46d73
Kate Basile <kathrynbasil5@gmail.com>
2025-04-10 16:16:13 -0400

    some updates

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable no-unused-vars */
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
